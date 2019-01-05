3907be36d962c69a82ec8c2b9b9fdad7be2be32e
ari <ari.maulana@harmony.co.id>
2019-01-06 06:54:15 +0700

    still runtime error on p621

--- uva/01_competitive_programming/problem_1_3_3/p621.cpp
+++ uva/01_competitive_programming/problem_1_3_3/p621.cpp
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <cstdio>
 #include <cstring>
 
 using namespace std;
 
 int main() {
     int T;
-    char s[101]; // because we aren't given the constraint limit of S digits, so assume it has max 100 char
+    char s[2001]; // because we aren't given the constraint limit of S digits, so assume it has max 2000 char
 
     if (scanf("%d", &T) != 1) return 1;
 
@@ -14,7 +14,8 @@ int main() {
 
         int len = strlen(s);
 
-        if (len > 2 && s[len - 2] == '3' && s[len - 1] == '5') printf("-\n");
+        if (strcmp(s, "1") == 0 || strcmp(s, "4") == 0 || strcmp(s, "78") == 0) printf("+\n");
+        else if (len > 2 && s[len - 2] == '3' && s[len - 1] == '5') printf("-\n");
         else if (len > 2 && s[0] == '9' && s[len - 1] == '4') printf("*\n");
         else if (len > 3 && s[0] == '1' && s[1] == '9' && s[2] == '0') printf("?\n");
         else printf("+\n");
