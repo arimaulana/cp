c4052dc90a75c456d7f2bf3c29bc6bb1e329a139
ari <ari.maulana@harmony.co.id>
2019-01-31 09:16:46 +0700

    already accepted in request for proposal debug file, but still got wrong answer. need to further debug for solving this problem

---
+++ uva/01_competitive_programming/problem_1_3_3/p10141.cpp
@@ -0,0 +1,69 @@
+#include <cstdio>
+#include <string>
+#include <map>
+
+using namespace std;
+typedef pair<string, int> psi;
+typedef map<string, int> msi;
+
+int main() {
+    int n, p, r;
+    int LEN = 82;
+    float d;
+
+    int i = 1;
+    while (scanf("%d %d\n", &n, &p) == 2, (n || p)) {
+        float compliance = 0;
+        float low_price = 0;
+        string vendor;
+
+        if (i > 1) puts("");
+        //printf("case = %d\tn = %d\tp = %d\n", i, n, p);
+
+        msi m;
+        char v[LEN];
+        string tmp_vendor;
+
+        int N = n;
+        while (N--) {
+            if (fgets(v, LEN, stdin) == NULL) {puts("error N"); return 1;}
+            
+            m.insert(psi(v, 1));
+        }
+
+        while (p--) {
+            r = 0;
+            float tmp_comp = 0;
+            int total_met = 0;
+            if (fgets(v, LEN, stdin) == NULL) {puts("error reading vendor"); return 1;}
+            tmp_vendor = v;
+
+            //printf("n = %d\tp = %d\tvendor = %sBefore d = %f\tr = %d\n", n, p, tmp_vendor.c_str(), d, r);
+            if (scanf("%f %d\n", &d, &r) != 2) {printf("error scanf, d = %f\t, r = %d\n", d, r); return 1;}
+            //printf("After, d = %f\tr = %d\n", d, r);
+            while (r--) {
+                if (fgets(v, LEN, stdin) == NULL) {puts("error r"); return 1;}
+
+                total_met = m[v] ? total_met + 1 : total_met;
+            }
+            
+            tmp_comp = total_met / (float)n;
+            if (tmp_comp > compliance) {
+                compliance = tmp_comp;
+                vendor = tmp_vendor;
+                low_price = d;
+            } else if (tmp_comp == compliance) {
+                if (d < low_price) {
+                    vendor = tmp_vendor;
+                    low_price = d;
+                }
+            }
+        }
+
+        printf("RFP #%d\n%s", i, vendor.c_str());
+        i++;
+    }
+
+    return 0;
+}
+
