48ae61d1a44e45782f9f35e1b9eae8c4e5e96b84
ari <ari.maulana@harmony.co.id>
2019-02-07 09:11:12 +0700

    Solve problem Request Proposals

--- uva/01_competitive_programming/problem_1_3_3/p10141.cpp
+++ uva/01_competitive_programming/problem_1_3_3/p10141.cpp
@@ -1,77 +1,93 @@
 #include <cstdio>
-#include <cstring>
 #include <string>
 #include <map>
 
 using namespace std;
+
 typedef pair<string, int> psi;
 typedef map<string, int> msi;
 
 int main() {
-    int n, p, r;
+    int n, p;
     int LEN = 82;
-    float d;
 
-    int i = 1;
-    while (scanf("%d %d\n", &n, &p) == 2, (n || p)) {
-        float compliance = 0;
-        float low_price = 0;
+    int t = 1;
+    while (scanf("%d %d\n", &n, &p) == 2 && (n || p)) {
+        if (t > 1) puts("");
+
+        // debug if p <= 0
+        if (p <= 0) return 1;
+    
         string vendor;
-
-        if (i > 1) puts("");
-        //printf("case = %d\tn = %d\tp = %d\n", i, n, p);
-
+        float comp = 0;
+        float price = 0;
+        
         msi m;
-        char v[LEN];
-        string tmp_vendor;
+        char s[LEN];
+        for (int i = 0; i < n; i++) {
+            if (fgets(s, LEN, stdin) == NULL) {printf("error getting requirements"); return 1;}
 
-        int N = n;
-        while (N--) {
-            if (fgets(v, LEN, stdin) == NULL) {puts("error N"); return 1;}
-            v[strcspn(v, "\n")] = 0;
-
-            //if (scanf("%[^\n]\n", v) != 1) {puts("error reading each requirement N"); return 1;}
-
-            m.insert(psi(v, 1));
+            m.insert(psi(s, 1));
         }
 
-        while (p--) {
-            r = 0;
+        for (int i = 0; i < p; i++) {
+            string tmp_vendor;
+            if (fgets(s, LEN, stdin) == NULL) {printf("error getting vendor of proposal"); return 1;}
+            tmp_vendor = s;
+
+            float d = 0;
+            int r = 0;
+            float met = 0;
             float tmp_comp = 0;
-            int total_met = 0;
-            if (fgets(v, LEN, stdin) == NULL) {puts("error reading vendor"); return 1;}
-            v[strcspn(v, "\n")] = 0;
-            
-            //if (scanf("%[^\n]\n", v) != 1) {puts("error reading vendor"); return 1;}
-            tmp_vendor = v;
 
-            //printf("n = %d\tp = %d\tvendor = %sBefore d = %f\tr = %d\n", n, p, tmp_vendor.c_str(), d, r);
-            if (scanf("%f %d\n", &d, &r) != 2) {printf("error scanf, d = %f\t, r = %d\n", d, r); return 1;}
-            //printf("After, d = %f\tr = %d\n", d, r);
-            while (r--) {
-                if (fgets(v, LEN, stdin) == NULL) {puts("error r"); return 1;}
-                v[strcspn(v, "\n")] = 0;
+            if (scanf("%f %d\n", &d, &r) != 2) {printf("error getting each vendor price and requirement"); return 1;}
 
-                //if (scanf("%[^\n]\n", v) != 1) {puts("error r"); return 1;}
+            for (int j = 0; j < r; j++) {
+                char tmp_s[LEN];
 
-                total_met = m[v] ? total_met + 1 : total_met;
+                if (fgets(tmp_s, LEN, stdin) == NULL) {printf("error getting requirement list"); return 1;}
+
+                met = m[tmp_s] == 1 ? met + 1 : met;
             }
-            
-            tmp_comp = total_met / (float)n;
-            if (tmp_comp > compliance) {
-                compliance = tmp_comp;
+
+            // Solution Part
+            //tmp_comp = met / n;
+            tmp_comp = (float)r / n;
+
+            // Debugging
+            //printf("State\tvendor = %s\tcomp = %f\tprice = %f\n", vendor.c_str(), comp, price);
+            //printf("Temp\tvendor = %s\tcomp = %f\tprice = %f\n", tmp_vendor.c_str(), tmp_comp, d);
+
+            if (vendor == "" && tmp_comp == 0) {
                 vendor = tmp_vendor;
-                low_price = d;
-            } else if (tmp_comp == compliance) {
-                if (d < low_price) {
-                    vendor = tmp_vendor;
-                    low_price = d;
-                }
+                comp = tmp_comp;
+                price = d;
+            } else if (tmp_comp > comp) {
+                vendor = tmp_vendor;
+                comp = tmp_comp;
+                price = d;
+            } else if (tmp_comp == comp && d < price) {
+                vendor = tmp_vendor;
+                comp = tmp_comp;
+                price = d;
+            } else if (tmp_comp == comp && d == price) {
+                // do nothing
+                //puts("all same");
+            } else if (tmp_comp < comp) {
+                // do nothing
+                //puts("temp comp less than comp");
+            } else if (tmp_comp == comp && d > price) {
+                // do nothing
+                //puts("d > price and temp comp same");
+            } else {
+                puts("some condition have not handled yet.");
+                return 1;
             }
+
         }
 
-        printf("RFP #%d\n%s\n", i, vendor.c_str());
-        i++;
+        printf("RFP #%d\n%s", t, vendor.c_str());
+        t++;
     }
 
     return 0;
