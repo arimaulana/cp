0dd91849eb984745d1e071e3ce55715133f564c9
arimaulana <maulana.ari@protonmail.com>
2022-10-13 14:56:51 +0700

    Add fix solution with java and buffer input

--- leetcode/problems/9-palindrome-number/solution.java
+++ leetcode/problems/9-palindrome-number/solution.java
@@ -1,6 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Solution {
-    public boolean isPalindrome(int x) {
+    public static boolean isPalindrome(int x) {
         if (x < 0) return false;
         if (x == 0) return true;
         if (x % 10 == 0) return false;
@@ -16,8 +19,16 @@ class Solution {
         return val == x;
     }
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(buf.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            int x = Integer.parseInt(buf.readLine());
+
+            System.out.printf("Case #%d:\n", tc);
+            System.out.println(isPalindrome(x));
+        }
     }
 }
 
