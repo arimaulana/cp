272f73499c1590f7d34e369ea838236a232bf779
arimaulana <maulana.ari@protonmail.com>
2023-06-01 14:20:25 +0700

    Add merge two sort

---
+++ leetcode/problems/21-merge-two-sorted-lists/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(): val(0), next(nullptr) {}
+    ListNode(int x): val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next): val(x), next(next) {}
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}
+
